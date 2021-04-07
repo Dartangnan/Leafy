82498f50e9243410969f90f993cb16b621aede46
Dartangnan Moreira Theml <dartangnan.theml@gmail.com>
2021-04-06 20:39:57 -0700

    openning animation with anime.js ready

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React, { useRef } from "react";
+import MainAnimation from "./MainAnimation";
+
+const App = () => {
+  return (
+    <div>
+      <MainAnimation />
+    </div>
+  );
+};
+
+export default App;
